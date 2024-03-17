e06007303b6a7fdf21be8af7d06d1374f382965d
Raphael <graphael.dr@gmail.com>
2024-03-17 14:40:17 -0600

    add new week and day views

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
 	import FullCalendar from '@fullcalendar/vue3'
 	import dayGridPlugin from '@fullcalendar/daygrid'
+	import timeGridPlugin from '@fullcalendar/timegrid'
 	import interactionPlugin from '@fullcalendar/interaction'
 
 	export default {
@@ -10,25 +11,35 @@
 		data() {
 			return {
 				calendarOptions: {
-					plugins: [dayGridPlugin, interactionPlugin],
+					plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
 					initialView: 'dayGridMonth',
 					dateClick: this.handleDateClick,
 					events: [
 						{ title: 'event 1', date: '2019-04-01' },
 						{ title: 'event 2', date: '2019-04-02' }
-					]
+					],
+					weekends: true,
+					headerToolbar: {
+						left: 'title',
+						center: 'dayGridMonth, timeGridWeek, timeGridDay',
+						right: 'prev,next'
+					}
 				}
 			}
 		},
 		methods: {
 			handleDateClick: function (arg) {
 				alert('date click! ' + arg.dateStr)
+			},
+			toggleWeekends: function () {
+				this.calendarOptions.weekends = !this.calendarOptions.weekends
 			}
 		}
 	}
 </script>
 
 <template>
+	<button @click="toggleWeekends">Toggle weekends</button>
 	<FullCalendar :options="calendarOptions" />
 </template>
 
